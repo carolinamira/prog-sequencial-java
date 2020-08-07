172bff3534914b6a28780fef212d1f74af87f873
carolinamira <carolinagmira@gmail.com>
2020-08-07 13:08:23 -0300

    versão 1

---
+++ ProgSequencial/src/Exercicios/Exercicio1.java
@@ -0,0 +1,31 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+//Exercício 1 - Programação Sequencial
+//Carolina Mira
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		int anos, meses, dias, diasfinal;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Informe quantos anos você tem:");
+		anos = ler.nextInt();
+		
+		System.out.println("\nInforme quantos meses se passaram desde seu último aniversário: ");
+		meses = ler.nextInt();
+		
+		System.out.println("\nInforme quantos dias se passaram neste mês, do dia em que vc nasceu até hoje: ");
+		dias = ler.nextInt();
+		
+		diasfinal = (anos*365) + (meses*30) + (dias*1);
+		System.out.println("\nVocê já viveu: " + diasfinal + " dias até agora. Parabéns :) " );
+		
+		ler.close();
+			
+	}
+	
+}
